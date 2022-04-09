c07b3d3c06323dee0426a1d380e0536a63d38e8e
Karol Jabłoński <karoljablonski373@gmail.com>
2022-04-09 20:51:25 +0200

    app add, delete tasks, count amount of tasks

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const insertNewTask = document.querySelector("div.app header form.insert_task in
 const submitNewTask = document.querySelector("div.app header form.insert_task button.add_task")
 const displayTasks = document.querySelector("div.app ul.tasks_list");
 const searchTask = document.querySelector("input.search");
+const tasksItems = document.getElementsByClassName("task_item");
 const numberOfTask = document.querySelector("footer p.number_of_tasks span");
-const tasksItems = document.querySelectorAll("div.app ul.tasks_list>li");
-
+let key = 0;
 //so we want to: get input after clicking '+', put that into needed 'box' which at last should be 'li' and add it to ul.tasks_list.
 //set up template to display task proper way to all tasks:
 function taskTemplate(item, text){
+    item.dataset.key = key;
+    key++;
     item.className = "task_item";
     item.innerHTML = `${text} <button class="remove_task">Usuń</button>`;
 };
@@ -23,7 +25,11 @@ function addNewTask(e){
     const newTaskBox = document.createElement("li");
     displayTasks.appendChild(newTaskBox);
     taskTemplate(newTaskBox, newTask);
+    newTaskBox.querySelector("button.remove_task").addEventListener("click", (e)=>{
+        e.target.parentNode.remove();
+    })
     insertNewTask.value = "";
+    numberOfTask.textContent = tasksItems.length;
     }
 }
 
