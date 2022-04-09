f11830950a3e8e78ee45bee1e7fd42c6ad9eb54e
Karol Jabłoński <karoljablonski373@gmail.com>
2022-04-09 17:19:57 +0200

    button do not push empty string as task, ul display tasks -last added on top of list-

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ const insertNewTask = document.querySelector("div.app header form.insert_task in
 const submitNewTask = document.querySelector("div.app header form.insert_task button.add_task")
 const displayTasks = document.querySelector("div.app ul.tasks_list");
 const searchTask = document.querySelector("input.search");
-const numberOfTask = document.querySelector("footer p.number_of_tasks");
+const numberOfTask = document.querySelector("footer p.number_of_tasks span");
+const tasksItems = document.querySelectorAll("div.app ul.tasks_list>li");
 
 //so we want to: get input after clicking '+', put that into needed 'box' which at last should be 'li' and add it to ul.tasks_list.
 //set up template to display task proper way to all tasks:
 function taskTemplate(item, text){
     item.className = "task_item";
     item.innerHTML = `${text} <button class="remove_task">Usuń</button>`;
-    item
 };
 
 //run after adding task:
 function addNewTask(e){
     e.preventDefault();
-    // console.log(taskTemplate("tu jest moj tekst!"));
+    if(insertNewTask.value == ""){
+        return;
+    } else {
     const newTask = insertNewTask.value;
     const newTaskBox = document.createElement("li");
     displayTasks.appendChild(newTaskBox);
     taskTemplate(newTaskBox, newTask);
     insertNewTask.value = "";
+    }
 }
 
 
