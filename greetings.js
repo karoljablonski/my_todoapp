558673314dd5c7e4d92ca0a65bb91018cfc119e4
Karol Jabłoński <karoljablonski373@gmail.com>
2022-04-11 20:19:48 +0200

    greeting page can be closed, added new js file, edited variables in css

---
+++ greetings.js
@@ -0,0 +1,6 @@
+const toApp = document.getElementById("toApp");
+const greetingsPage = document.querySelector("div.greetings");
+toApp.addEventListener("click", ()=>{
+    greetingsPage.style.display = "none";
+    //dodac jakas animacje prostą :)
+})
