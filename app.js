b0229d5b02c04ce58c5f04678e1ccf47ff69f062
Karol Jabłoński <karoljablonski373@gmail.com>
2022-04-12 19:57:36 +0200

    app.js cleaned up

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const searchTask = document.querySelector("input.search");
 const tasksItems = document.getElementsByClassName("task_item");
 const numberofTaskSentence = document.querySelector("footer>p.number_of_tasks");
 let key = 0;
-//so we want to: get input after clicking '+', put that into needed 'box' which at last should be 'li' and add it to ul.tasks_list.
-//set up template to display task proper way to all tasks:
+
+//proper set dataset for tasks:
 function setDataKeyOnTask(){
     key = 0;
     const tasksItemsConverted = [...tasksItems];
@@ -17,16 +17,15 @@ function setDataKeyOnTask(){
     for(let i = 0; i < tasksItemsConverted.length; i++){
         tasksItemsConverted[i].dataset.key = i;
     }
-    
 }
 
+//set up template to display task proper way to all tasks:
 function taskTemplate(item, text){
-    // item.dataset.key = key;
-    // key++;
     item.className = "task_item";
     item.innerHTML = `${text} <button class="remove_task">Done!</button>`;
 };
 
+//info displayed about number of tasks added:
 function howManyTasks(){
     const num = tasksItems.length;
     const statement = numberofTaskSentence;
@@ -39,7 +38,7 @@ function howManyTasks(){
     }
 }
 
-//run after adding task:
+//add task, check if empty string, append new list item, set its classes, html, datakey, update number of tasks, when removed - recalculate:
 function addNewTask(e){
     e.preventDefault();
     if(insertNewTask.value == ""){
@@ -57,20 +56,19 @@ function addNewTask(e){
                 howManyTasks();
                 setDataKeyOnTask();
         });
-};
-insertNewTask.value = "";
+    };
+    insertNewTask.value = "";
 }
-
+//button add task:
 submitNewTask.addEventListener("click", addNewTask);
 
 //searching:
 searchTask.addEventListener("input", ()=>{
     const mySearch = searchTask.value.toLowerCase();
-    let myList = [...tasksItems];
-    let shortenedItem; 
-    myList.forEach((item)=>{
+    let tasksItemsConverted = [...tasksItems];
+    tasksItemsConverted.forEach((item)=>{
         item.style.display = "none";
     });
-    myList = myList.filter(item=>item.textContent.slice(0, (item.textContent.length-5)).toLowerCase().includes(mySearch)); //gets text from item, slices last 5 chars, changes letters to lower, and checks if includes any char from mySearch.
-    myList.forEach(item=>item.style.display = "list-item");
+    tasksItemsConverted = tasksItemsConverted.filter(item=>item.textContent.slice(0, (item.textContent.length-5)).toLowerCase().includes(mySearch)); //gets text from item, slices last 5 chars, changes letters to lower, and checks if includes any char from mySearch.
+    tasksItemsConverted.forEach(item=>item.style.display = "list-item");
     });
